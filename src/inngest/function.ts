1f8de19c8ce06d4535bcc8aecda0a07e94378dde
iVishav48 <vishav4848@gmail.com>
2025-11-03 15:42:36 +0530

    feat: Integrate OpenAI SDK

--- src/inngest/function.ts
+++ src/inngest/function.ts
@@ -1,10 +1,33 @@
 import { inngest } from "./client";
+import { createOpenAI } from "@ai-sdk/openai";
 
-export const helloWorld = inngest.createFunction(
-  { id: "hello-world" },
-  { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
-  },
-);
+import { generateText } from "ai";
+
+const openai = createOpenAI();
+
+
+export const execute = inngest.createFunction(
+    { id: "execute-ai" },
+    { event: "execute/ai" },
+    async ({ event, step }) => {
+
+
+        const { steps: openaiSteps } = await step.ai.wrap(
+            "openai-generate-text",
+            generateText,
+            {
+                model: openai("gpt-4.1"),
+                system: "You are a helpful assistant",
+                prompt: "what is 2+2",
+                experimental_telemetry: {
+                    isEnabled: true,
+                    recordInputs: true,
+                    recordOutputs: true,
+                }
+            }
+        );
+        return {
+            openaiSteps,
+        }
+    }
+)
